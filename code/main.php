95c5f6b225e908d4f695859b3a84c5a0f7388657
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2026-09-02 13:47:42 -0400

    Update files

--- code/main.php
+++ code/main.php
@@ -6,4 +6,10 @@ require __DIR__ . '/vendor/autoload.php';
 
 $data = Yaml::parseFile('config.yml');
 
-print_r($data);
+//print_r($data);
+
+print_r($data['parish']);
+
+echo "\n==================================\n";
+
+print_r($data['baptisms']);
