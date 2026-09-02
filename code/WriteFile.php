57981f0ab0d9d8743ade1bb21cf11a067e03363c
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2026-09-02 09:25:27 -0400

    Update files

--- code/WriteFile.php
+++ code/WriteFile.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 class WriteFile {
 
 	private $key_values = array();
+
 	private $parts = array();
 	
 	private function tokenize(string $str) : array
@@ -19,14 +20,16 @@ class WriteFile {
           return $parts;
 	}
 
+	private static $md_template = ...
+
 	public function __construct(array $keyValues) 
 	{
-	   $this->parts = $this->tokenize($self::$page);
+	   $this->parts = $this->tokenize($self::$md_tempalte);
 	}
 
-	public function __invoke(string $filename, array $subKeys) 
+	public function __invoke(string $filename, array $yamlRecords) 
 	{
-           $this->key_values = $this->xyz($subKeys);
+           $this->key_values = $this->xyz($yamlRecords);
 
            $this->write($filename);  	   
 	}
