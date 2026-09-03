d4f30e777c14dc7fe4857e8b1407447ca2bee6a4
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2026-09-03 19:43:50 -0400

    Update files

--- code/main.php
+++ code/main.php
@@ -15,7 +15,7 @@ $yaml = Yaml::parseFile('config.yml');
 
 $parish_settings = array_slice($yaml['parish'], 0, 3);
 
-$current_citation = str_replace(array("@path", "@parish-name"),
+$citation_str = str_replace(array("@path", "@parish-name"),
 	array($parish_settings['volumes']['path'],
               $parish_settings['parish-name']),
 	$citation_str); 
@@ -27,10 +27,21 @@ foreach ($sections as $section_key) {
     $parish_volumes = $yaml['parish']['volumes'];
         
     $section = $yaml['parish'][$section_key];
+    
+    $current_md = $md_string;
+    
+    $current_citation =  $citation_str;
+    
+    // TODO: add all the variables in template.md to this array.
+    $md_find_array = array('%year', '%name', '@event', '&file-name', '%edate');
      
     foreach ($section['records'] as $record) {
       
           print_r($record);
+          
+          $current_md = str_replace($md_find_array, $record, $current_md);
+          
+          //$current_citation = str_replace(,, $current_citation);
           
           echo "\n================\n";
     }    
