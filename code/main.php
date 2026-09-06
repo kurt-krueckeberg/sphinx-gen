3d6ed2c349e389d8fd8c4fc826fe46adf913051f
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2026-09-06 12:14:32 -0400

    Update files

--- code/main.php
+++ code/main.php
@@ -8,18 +8,18 @@ require __DIR__ . '/vendor/autoload.php';
 
 $folder = "/home/kurt/sphinx-gen/code";
 
-$citation_string = file_get_contents($folder . "/citation.md");
-        
-$md_string = file_get_contents($folder . "/template.md");
-
 $kbr = new KirchenbuecherResults("/home/kurt/sphinx-gen/code/config.yml");
 
 $parish_settings = $kbr->getParishValues();
+
+$citation_string = file_get_contents($folder . "/citation.md");
 
 $citation_string = str_replace(array("@path", "@parish-name"),
 	array($parish_settings['volumes']['path'],
               $parish_settings['parish-name']),
   	      $citation_string); 
+        
+$md_string = file_get_contents($folder . "/template.md");
 
 $markdown_template = $md_string . $citation_string;
 
--- code/src/MystMD/MarkdownCreator.php
+++ code/src/MystMD/MarkdownCreator.php
@@ -1,6 +1,6 @@
 <?php
 declare (strict_types=1);
-namespace myst_gen;
+namespace MystMD;
 
 class MarkdownCreator {
 
