afc67e9ad7ddf3f9888778b26f59910283b250a9
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2026-09-05 21:16:56 -0400

    Update files

--- code/main.php
+++ code/main.php
@@ -8,64 +8,29 @@ use Symfony\Component\Yaml\Yaml;
 
 require __DIR__ . '/vendor/autoload.php';
 
-function create_filename(string $prefix, string $symbol, string $year) : string
-{
-   $filestem = $prefix . '-' . $symbol . '-' . $year;
-
-   for($i = 0; 1; ++$i) {
-
-       $filename =  $filestem . (char) ('a' + $i)  . "md";
-
-       if (file_exists($filename))
-		   continue;
-
-       return $filestem;
-    }	       
-}
-
-
-$citation_str = Config::get_config()->citation_string;
 
 $md_string = Config::get_config()->md_string;
 
-$yaml = Yaml::parseFile('config.yml');
-
-$parish_settings = array_slice($yaml['parish'], 0, 3);
+$citation_str = Config::get_config()->citation_string;
 
 $citation_str = str_replace(array("@path", "@parish-name"),
 	array($parish_settings['volumes']['path'],
               $parish_settings['parish-name']),
 	$citation_str); 
 
-$sections = array("marriages", "burials", "confirmations", "baptisms");
 
-foreach ($sections as $section_key) {
 
-    $parish_volumes = $yaml['parish']['volumes'];
-        
-    $section = $yaml['parish'][$section_key];
-    
-    $current_md = $md_string;
-    
-    $current_citation =  $citation_str;
-    
-    // TODO: add all the variables in template.md to this array.
-    $md_find_array = array('%year', '%name', '@event', '&file-name', '%edate');
-     
-    foreach ($section['records'] as $record) {
+$ceremony_section = 
+foreach ($ceremony_section as $record) {
       
-          print_r($record);
-
-	  $filename = create_filename($parish_settings['prefix'], $record['edate']['record-symbol'],
-		  substr($record['edate'], strrchr($record['edate'], ' ') + 1));
+    print_r($record);
           
-          $current_md = str_replace($md_find_array, $record, $current_md);
+    $current_md = str_replace($md_find_array, $record, $current_md);
           
-          //$current_citation = str_replace(,, $current_citation);
+    //$current_citation = str_replace(,, $current_citation);
           
-          echo "\n================\n";
-    }    
-}
+    echo "\n================\n";
+}    
 
 
 
