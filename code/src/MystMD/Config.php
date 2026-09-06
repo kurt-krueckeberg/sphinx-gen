0129df859354b34731eed689d11804b1fb60e7d1
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2026-09-06 10:05:48 -0400

    Update files

--- code/src/MystMD/Config.php
+++ code/src/MystMD/Config.php
@@ -8,11 +8,11 @@ class config_ {
     
     public readonly string $md_string;
     
-    public function __construct()
+    public function __construct(string $folder)
     {
-        $this->citation_string = file_get_contents("citation.md");
+        $this->citation_string = file_get_contents($folder . "/citation.md");
         
-        $this->md_string = file_get_contents("template.md");
+        $this->md_string = file_get_contents($folder . "/template.md");
     }
 }
 
@@ -21,20 +21,31 @@ class Config {
   private static config_ $c;  
   private static bool $initialized = false;
 
-   static public function get_config() : config_
+   static private function get_config(string $folder) : config_
    {
       if (self::$initialized === false) {
        
-          self::$c = new config_;
+          self::$c = new config_($folder);
      
           self::$initialized = true;
       }
     
       return self::$c;
+   }
+
+  public function __construct(string $folder)
+  {
+     self::get_config($folder);	  
   }
 
-  public function test() : config_
+  public function get_citation_string() : string
   {
-      return self::get_config();	  
+      return self::get_config()->citation_string;	  
   }
+
+  public function get_markdown_string() : string
+  {
+      return self::get_config()->md_string;	  
+  }
+
 }
