d34df676d96f9f3f51ce6dd519acebbded35090e
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2026-09-09 10:17:40 -0400

    Update files

--- code/main.php
+++ code/main.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use Symfony\Component\Yaml\Yaml;
 use MystMD\{KirchenBuecherResults, Config, CeremonySection, MarkdownCreator, FileLogger};       
 
 require __DIR__ . '/vendor/autoload.php';
